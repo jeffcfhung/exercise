dc2484ca0128b82e0667b70763a0527b6086bb63
Jeff Hung <jhung@Jeff-Hung-MacBook-Air.local>
2018-12-08 20:57:04 -0800

    Updating comments

--- Report.java
+++ Report.java
@@ -27,6 +27,7 @@ class Report {
       return result;
     }
     
+    // Read file content line by line to prevent running out of memory
     while (in.hasNext()) {
       String line = in.nextLine();
       String[] tokens = line.split("\\|");
@@ -34,6 +35,7 @@ class Report {
         errorCount++;
         continue;
       }
+      // Round the time to beginning of date
       Long roundedToDateTime = (Long.parseLong(tokens[0])/86400L)*86400L;
       HashMap<String, Integer> urlMap;
       if (result.containsKey(roundedToDateTime)) {
@@ -44,6 +46,7 @@ class Report {
         result.put(roundedToDateTime, urlMap);
       }
 
+      // Increase hit count of URL
       String url = tokens[1];
       int count = urlMap.getOrDefault(url, 0);
       urlMap.put(url, count+1);
@@ -60,6 +63,7 @@ class Report {
       DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
       for (Long date : allUrlHits.keySet()) {
         LocalDateTime dateTime = LocalDateTime.ofEpochSecond(date, 0, ZoneOffset.UTC);
+        // Print date for report  
         System.out.printf("%s GMT%n", dateTime.toLocalDate().format(formatter));
 
         HashMap<String, Integer> dailyUrlHits = allUrlHits.get(date);
@@ -67,6 +71,7 @@ class Report {
         List<Entry<String, Integer>> sortedList = new ArrayList<>(dailyUrlHits.entrySet());
         sortedList.sort(Collections.reverseOrder(Entry.comparingByValue()));
 
+        // Print url and hit count for report
         for (Entry<String, Integer> e: sortedList) {
           System.out.printf("%s %d%n", e.getKey(), e.getValue());
         }
@@ -74,15 +79,16 @@ class Report {
   }
 
   // NOTE:
-  // Time complexity is mlog(m), m<=n because of sorting for hits count.
-  // memory complexity is O(n)
+  // Time complexity of sorting is m*log(m), m<=n, m is the maximum number of URLs to be sorted in one day
+  // Time complexity of TreeMap get and put functions is n*log(n). So the most signficant time complexity shall be n*log(n)
+  // Memory complexity is O(n) due to HashMap memory usage
   
   public static void main(String[] args) {
     if (args.length < 1) {
       System.out.printf("Please specify the file name to proceed%n");
       return;
     }
-    // TODO: Add unit tests
+    // TODO: Add unit tests for postive and negative test cases
     TreeMap<Long, HashMap<String, Integer>> allUrlHits = processUrlHits(args[0]);
     generateReport(allUrlHits);
   } 
